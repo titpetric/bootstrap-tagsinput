5e926de7c260ad4cebf7d679230a86c256e6c954
Tit Petric <tit.petric@monotek.net>
2017-04-05 13:37:37 +0200

    add case sensitivity

--- src/bootstrap-tagsinput.js
+++ src/bootstrap-tagsinput.js
@@ -28,6 +28,7 @@
     },
     trimValue: false,
     allowDuplicates: false,
+    caseSensitive: true,
     triggerChange: true
   };
 
@@ -110,7 +111,7 @@
           itemTitle = self.options.itemTitle(item);
 
       // Ignore items allready added
-      var existing = $.grep(self.itemsArray, function(item) { return self.options.itemValue(item) === itemValue; } )[0];
+      var existing = $.grep(self.itemsArray, function(item) { return self.options.caseSensitive?self.options.itemValue(item) === itemValue:self.options.itemValue(item).toLowerCase() === itemValue.toLowerCase(); } )[0];
       if (existing && !self.options.allowDuplicates) {
         // Invoke onTagExists
         if (self.options.onTagExists) {
